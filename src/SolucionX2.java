b18003702afe995cbea49c7a5eaa7b1c42c62ff7
nayeli92433 <nayeserratvl@gmail.com>
2023-05-02 17:09:19 -0500

    Actividad 9

--- src/SolucionX2.java
+++ src/SolucionX2.java
@@ -2,7 +2,6 @@ public class SolucionX2 {
     int resultadoX2;
     public int sustitucionX2(int a , int b, int c){
         resultadoX2= (int) (-b - Math.sqrt(Math.pow(b, 2)-4*(a*c)))/(2*a);
-        //System.out.println(resultadoX2);
         return resultadoX2;
     }
 }
